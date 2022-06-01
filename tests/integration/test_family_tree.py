dbf9e3462953ecbc19d564da4d32371740d1a093
AyaloL <ayalon@skylinerobotics.com>
2022-06-01 14:21:11 +0300

    Integration and unit tests for family_tree.py added

--- tests/integration/test_family_tree.py
+++ tests/integration/test_family_tree.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from family_tree.member import Member
+from family_tree.member import Member, Gender
 from family_tree.family_tree import FamilyTree
 
 
@@ -9,13 +9,13 @@ class TestFamilyTree(TestCase):
     def setUp(self):
         self.ftree = FamilyTree()
 
+
     def test_add_child(self):
         result = self.ftree.add_child("Father", "Male")
         self.assertEqual(result, "CHILD_ADDITION_SUCCEEDED", "Family tree is not empty")
-        self.assertEqual(self.ftree.family_tree.get("Father", None) is not None, True, "Father is not in family tree")
 
         self.assertEqual(self.ftree.add_child("Child2", "Female", "Mother"), "PERSON_NOT_FOUND",
-                         "Apperantly there is a mother, who knew?")
+                         "Apparently there is a mother, who knew?")
         self.assertEqual(self.ftree.add_child("Child2", "Female", "Father"), "CHILD_ADDITION_FAILED",
                          "Father cannot have a child by himself...")
 
@@ -26,8 +26,61 @@ class TestFamilyTree(TestCase):
 
         self.assertEqual(self.ftree.add_child("Child2", "Female", "Mother"), "CHILD_ADDITION_SUCCEEDED",
                          "Child wasn't add to family tree")
+
+        # check that a child cannot be added more than once to family tree
+        self.assertEqual(self.ftree.add_child("Child2", "Female", "Mother"), "CHILD_ADDITION_FAILED",
+                         "Duplicate child added to family tree")
+
         self.assertEqual(self.ftree.family_tree.get("Child2", None) is not None, True, "Child is not in family tree")
 
         print("Test MTF_IT_0003 ----> PASSED")
 
+    def test_add_spouse(self):
+        self.assertEqual(self.ftree.add_spouse("Spouse", "Female", "Member"), "SPOUSE_ADDITION_FAILED",
+                         "Unknown member in family tree")
+
+        dummy_member = Member(1, "DummyMember", "Male")
+        self.ftree.family_tree["DummyMember"] = dummy_member
+        self.assertEqual(self.ftree.add_spouse("Spouse", "Female", "Member"), "PERSON_NOT_FOUND",
+                         "Unknown member in family tree")
+
+        spouse_a = Member(1, "FakeMember", "Female")
+        spouse_b = Member(2, "MarriedMember", "Male")
+        spouse_b.set_spouse(spouse_a)
+        spouse_c = Member(3, "Member", "Male")
+        self.ftree.family_tree["FakeMember"] = spouse_a
+        self.ftree.family_tree["MarriedMember"] = spouse_b
+        self.ftree.family_tree["Member"] = spouse_c
+
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "FakeMember"), "SPOUSE_ADDITION_FAILED",
+                         "Spouse has sam gender - PROGRESS YAY!!!")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "MarriedMember"), "SPOUSE_ADDITION_FAILED",
+                         "Spouse is married to twice")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "Member"), "SPOUSE_ADDITION_SUCCEEDED",
+                         "The marriage didn't happen")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "Member"), "SPOUSE_ADDITION_FAILED",
+                         "2 identical Spouses")
+
+        print("Test MTF_IT_0004 ----> PASSED")
+
+    def test_get_relationship(self):
+        self.assertEqual(self.ftree.get_relationship("Member", "brothers_in_law"), "PERSON_NOT_FOUND",
+                         "Unknown person in family tree")
+        # setup for test
+        member = Member(1, "Member", "Male")
+        son_a = Member(2, "Son_a", "Male")
+        son_b = Member(3, "Son_b", "Male")
+        member.add_child(son_a)
+        member.add_child(son_b)
+        son_a.set_father(member)
+        son_b.set_father(member)
+        self.ftree.family_tree["Member"] = member
+        self.ftree.family_tree["Son_b"] = son_b
+        self.ftree.family_tree["Son_a"] = son_a
+
+        self.assertEqual(self.ftree.get_relationship("Member", "daughters"), "NONE", "Unknown daughters")
+        self.assertEqual(self.ftree.get_relationship("Member", "sons"), ["Son_a", "Son_b"],
+                         "Sons were not found")
+
+        print("Test MTF_IT_0004 ----> PASSED")
 
--- tests/unit/test_family_tree.py
+++ tests/unit/test_family_tree.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch, Mock
 
 from family_tree.family_tree import FamilyTree
-from family_tree.member import Gender
+from family_tree.member import Member, Gender
 from tests.unit import create_fake_member
 
 
@@ -29,11 +29,11 @@ class TestFamilyTree(TestCase):
         # if either mother or father don't exist
         mother = create_fake_member(id=2, name="Mother", gender=Gender.female)
         father = create_fake_member(id=3, name="Father", gender=Gender.male)
-        fakemother = create_fake_member(id=4, name="FakeMother", gender=Gender.male)
+        fake_mother = create_fake_member(id=4, name="FakeMother", gender=Gender.male)
 
         self.assertEqual(self.ftree.add_child("ChildName2", "Male", "Mother"), "PERSON_NOT_FOUND", "Unexpected mother")
 
-        self.ftree.family_tree['FakeMother'] = fakemother
+        self.ftree.family_tree['FakeMother'] = fake_mother
         self.assertEqual(self.ftree.add_child("ChildName2", "Male", "FakeMother"), "CHILD_ADDITION_FAILED",
                          "Hi! This is not a real mother")
 
@@ -47,7 +47,64 @@ class TestFamilyTree(TestCase):
 
         self.assertEqual(self.ftree.add_child("ChildName2", "Male", "Mother"), "CHILD_ADDITION_SUCCEEDED",
                          "Child was not added to family tree")
+
+        # check that a child cannot be added more than once to family tree
+        self.assertEqual(self.ftree.add_child("ChildName2", "Male", "Mother"), "CHILD_ADDITION_FAILED",
+                         "Duplicate child added to family tree")
+
         self.assertEqual(isinstance(self.ftree.family_tree.get("ChildName2", None), Mock), True,
                          "Child2 is not part of family tree")
 
         print("Test MTF_UT_0020 ----> PASSED")
+
+    @patch('family_tree.family_tree.Member', return_value=create_fake_member(id=1, name="Spouse", gender=Gender.female))
+    def test_add_spouse(self, mock_member):
+        # if tree is empty
+        result = self.ftree.add_spouse("Spouse", "Female", "Member")
+        mock_member.assert_called_with(1, "Spouse", "Female")
+
+        self.assertEqual(self.ftree.family_tree.get("Member", None), None,
+                         "Family Tree supposed to be empty but it is not")
+        self.assertEqual(result, 'SPOUSE_ADDITION_FAILED', "Apparently there is a spouse without a second half")
+
+        # if spouse don't exist
+        # First we create a dummy member to make sure that the family tree is not empty
+        dummy_member = create_fake_member(id=0, name="DummyMember", gender=Gender.male)
+        self.ftree.family_tree["DummyMember"] = dummy_member
+
+        spouse_a = create_fake_member(id=2, name="Member", gender=Gender.male)
+        spouse_b = create_fake_member(id=3, name="FakeMember", gender=Gender.female)
+        spouse_c = create_fake_member(id=4, name="MarriedMember", gender=Gender.male, spouse=spouse_b)
+
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "Member"), "PERSON_NOT_FOUND",
+                         "Spouse")
+        self.ftree.family_tree["Member"] = spouse_a
+        self.ftree.family_tree["FakeMember"] = spouse_b
+        self.ftree.family_tree["MarriedMember"] = spouse_c
+
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "FakeMember"), "SPOUSE_ADDITION_FAILED",
+                         "Spouse has sam gender - PROGRESS YAY!!!")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "MarriedMember"), "SPOUSE_ADDITION_FAILED",
+                         "Spouse is married to twice")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "Member"), "SPOUSE_ADDITION_SUCCEEDED",
+                         "The marriage didn't happen")
+        self.assertEqual(self.ftree.add_spouse("Spouse", Gender.female, "Member"), "SPOUSE_ADDITION_FAILED",
+                         "2 identical Spouses")
+
+        print("Test MTF_UT_0021 ----> PASSED")
+
+    @patch('family_tree.family_tree.Member.get_relationship', side_effect=[
+        [],
+        [create_fake_member(id=1, name="Member"), create_fake_member(id=2, name="Spouse")]
+    ])
+    def test_get_relationship(self, mock_get_relationship):
+        self.assertEqual(self.ftree.get_relationship("Member", "brothers_in_Law"), "PERSON_NOT_FOUND",
+                         "Unknown person in family tree")
+
+        self.ftree.family_tree["Member"] = Member(1, "Member", "Male")
+        self.assertEqual(self.ftree.get_relationship("Member", "brothers_in_Law"), "NONE",
+                         "There are some unexpected brothers-in-lay")
+        self.assertEqual(self.ftree.get_relationship("Member", "brothers_in_Law"), ["Member", "Spouse"],
+                         "Unknown person in family tree")
+
+        print("Test MTF_UT_0021 ----> PASSED")
